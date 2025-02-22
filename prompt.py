51b9a910a944f5ee514661651b0a7f07834cc751
James Tan <jamesruperttan@gmail.com>
2025-02-22 12:28:36 -0500

    Add stocks to prompt

--- prompt.py
+++ prompt.py
@@ -1,15 +1,16 @@
 import requests
 from dotenv import dotenv_values
+from time import sleep
 
 config = dotenv_values(".env")
 
-resp = requests.get(f"https://newsapi.org/v2/everything?apiKey={config["NEWSAPI"]}&from=2025-02-20&sources=bloomberg,fortune,business-insider,reuters,the-wall-street-journal")
+resp = requests.get(f"https://newsapi.org/v2/everything?apiKey={config['NEWSAPI']}&from=2025-02-20&sources=bloomberg,fortune,business-insider,reuters,the-wall-street-journal")
 json = resp.json()
 articles = json['articles']
 headlines = ["- " + x["title"] for x in articles]
 headlines = '\n'.join(headlines)
 
-resp = requests.get(f"https://api.congress.gov/v3/bill?api_key={config["CONGRESSGOV"]}&limit=100&fromDateTime=2025-02-21T00:00:00Z")
+resp = requests.get(f"https://api.congress.gov/v3/bill?api_key={config['CONGRESSGOV']}&limit=100&fromDateTime=2025-02-21T00:00:00Z")
 json = resp.json()
 newbills = ["- " + x["title"] for x in json['bills']]
 newbills = '\n'.join(newbills)
@@ -26,6 +27,20 @@ tickers = [
   "MSFT",
   "AAPL",
 ]
+tickers = tickers[:5]
+
+def get_ticker(t):
+    resp = requests.get(f"https://api.polygon.io//v2/aggs/ticker/{t}/prev?apiKey={config['POLYGONAPI']}")
+    json = resp.json()
+    keys = ['o', 'c', 'h', 'l', 'n']
+    print(json)
+    stock = {k: json['results'][0][k] for k in keys}
+    stock["ticker"] = json["ticker"]
+    #sleep(15)
+    return stock
+stocks = [get_ticker(t) for t in tickers]
+stocks = [f"- {s['ticker']} (open: {s['o']}, close: {s['c']}, high: {s['h']}, low: {s['l']}, transactions: {s['n']})" for s in stocks]
+stocks = '\n'.join(stocks)
 
 prompt = f"""# Introduction
 You are acting as a top-tier financial trader.
@@ -43,9 +58,9 @@ Use positive counts to signify a buy, and negative counts for a sell.
 
 # Provided information
 You will receive the following information: recent news headlines, recently modified laws,
-stock tickers as well as their end-of-market prices for each day in the past two weeks,
+historical stock data from the previous day with tickers as well as
+their open price, close price, highest price, lowest price, and number of transactions,
 and currently held stock tickers with their quantities.
-Only use stocks which are listed in the historical data.
 
 ## Recent news headlines
 {headlines}
@@ -54,6 +69,7 @@ Only use stocks which are listed in the historical data.
 {newbills}
 
 ## Historical stock data
+{stocks}
 
 ## Currently held stocks
 """
